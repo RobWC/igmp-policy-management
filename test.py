1b4c43519068f90869fc09fcd18a1aa56fbdedd7
Rob Cameron <rwcameron@gmail.com>
2014-11-26 12:35:43 -0800

    objectified code

--- test.py
+++ test.py
@@ -2,8 +2,9 @@ from pprint import pprint
 from jnpr.junos import Device
 from jnpr.junos.cfg.resource import Resource
 from jnpr.junos.utils.config import Config
+from jinja2 import Template
 
-igmp_policy_template = '''
+igmp_policy_template1 = '''
 policy-options {
     policy-statement igmp-allowed {
         term allow-customer-1 {
@@ -28,26 +29,50 @@ policy-options {
 }
 '''
 
-dev = Device(host="172.16.237.128",user="root",password="Juniper")
-dev.open()
+igmp_policy_template = '''
+policy-options {
+    policy-statement igmp-allowed {
+        term allow-{{ customer_name }}-1 {
+            from {
+                route-filter {{ group_addr }} exact;
+                source-address-filter {{ source_addr }} exact;
+            }
+            then accept;
+        }
+        term allow-{{ customer_name }}-2 {
+            from {
+                {% for route in routes %}
+                route-filter {{ route }} exact;
+                {% endfor %}
+                {% if source_address_filter is defined %}
+                source-address-filter {{ source_address_filter }} exact;
+                {% endif %}
+            }
+            then accept;
+        }
+        term deny-everything-else {
+            then reject;
+        }
+    }
+}
+'''
 
-dev.bind(cu=Config)
+dev = Device(host="10.0.1.234",user="root",password="Juniper")
+dev.open()
 
 pprint(dev.facts)
 
 try:
+    #attempt to open a configuration
     output = dev.rpc("<open-configuration><private/></open-configuration>")
 except Exception as err:
+    #output an error if the configuration is not availble
     print err
 
 try:
-    output = dev.cu.load(igmp_policy_template,format="text",merge=True)
+    output = dev.cu.load(igmp_policy_template1,format="text",merge=True)
 except Exception as err:
     print err
-
-#output = dev.rpc("<commit-configuration></commit-configuration>")
-
-#print output
 
 output = dev.rpc.commit_configuration()
 
@@ -57,7 +82,29 @@ print output
 
 dev.close()
 
+class Manager():
+    def __init__(self,host,user,password)
+        self.dev = Device(host="10.0.1.234",user="root",password="Juniper")
+        self.dev.bind(cu=Config)
+
+    def open(self):
+        self.dev.open()
+
+    def load_config_template(self,template,template_vars)
+        new_template = Template(template)
+        final_template = new_template.render(template_vars)
+
+        try:
+            output = dev.cu.load(final_template,format="text",merge=True)
+        except Exception as err:
+            print err
+
+    def commit_config(self):
+        self.dev.rpc.commit_configuration()
+        self.dev.rpc.close_configuration()
+
+
 #apply igmp group to interface
 #create igmp group
 #open an ephemeral config
-#standard commits
+#standard commit
